b8cc34b562351ff0256bcf999212983ceeb44206
Peter Rathgeb <peter.rathgeb@htl-steyr.ac.at>
2020-11-18 10:29:36 +0100

    Initial commit

---
+++ src/Launcher.java
@@ -0,0 +1,13 @@
+public class Launcher {
+
+    /**
+     * @Todo
+     * Click the green "play" - icon to run the application.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    /**
+     * @ToDo edit run configuration
+     * !! FIRST TRY TO RUN YOU APPLICATION FROM THE LAUNCHER CLASS !!
+     *
+     *
+     *
+     * If this does not work try the following:
+     * Open "Run" -> "Edit Configurations..."
+     * Add this to your VM options (just copy & paste the following line):
+     * --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls,javafx.fxml
+     */
+
+    /**
+     * main method
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
